2aaa57cbf1f52d01adf61415c8e8f0b0b6f1aecc
topu <topukhan735@gmail.com>
2023-10-21 19:47:51 +0600

    Without JS, just rest API

--- routes/web.php
+++ routes/web.php
@@ -16,37 +16,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-Route::get('/setup', function () {
-    $credentials = [
-        'email' => 'admin@gmail.com',
-        'password' => '12345678'
-    ];
+// Route::get('/setup', function () {
+//     $credentials = [
+//         'email' => 'admin@gmail.com',
+//         'password' => '12345678'
+//     ];
 
-    if(!Auth::attempt($credentials)){
-        $user = new User();
+//     if(!Auth::attempt($credentials)){
+//         $user = new User();
 
-        $user->name = 'admin';
-        $user->email = $credentials['email'];
-        $user->password = Hash::make($credentials['password']);
+//         $user->name = 'admin';
+//         $user->email = $credentials['email'];
+//         $user->password = Hash::make($credentials['password']);
 
-        $user->save();
+//         $user->save();
 
-        if(Auth::attempt($credentials)){
-            $user = Auth::user();
+//         if(Auth::attempt($credentials)){
+//             $user = Auth::user();
 
-            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
-            $updateToken = $user->createToken('update-token', ['create', 'update']);
-            $basicToken = $user->createToken('basic-token');
+//             $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+//             $updateToken = $user->createToken('update-token', ['create', 'update']);
+//             $basicToken = $user->createToken('basic-token');
 
-            return [
-                'admin' => $adminToken->plainTextToken,
-                'update' => $updateToken->plainTextToken,
-                'basic' => $basicToken->plainTextToken,
-            ];
-        }
-    }
-});
+//             return [
+//                 'admin' => $adminToken->plainTextToken,
+//                 'update' => $updateToken->plainTextToken,
+//                 'basic' => $basicToken->plainTextToken,
+//             ];
+//         }
+//     }
+// });
+
+Route::view('/customers', 'page.customers');
